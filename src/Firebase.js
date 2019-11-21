168e6691648d9298b032fb9458c74e28b6076667
KevinKipkemei <42734616+KevinKipkemei@users.noreply.github.com>
2019-11-21 16:55:02 +0300

    Secure firebase deleted key

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,14 +1,14 @@
  import firebase from "firebase";
 
  const firebaseApp = firebase.initializeApp ({
-    apiKey: "AIzaSyCkE-xmQualBe0m713t3wf8zPr5U5SChYE",
-    authDomain: "field-d538a.firebaseapp.com",
-    databaseURL: "https://field-d538a.firebaseio.com",
-    projectId: "field-d538a",
-    storageBucket: "field-d538a.appspot.com",
-    messagingSenderId: "772294768735",
-    appId: "1:772294768735:web:1e2c558a981215af6583ea",
-    measurementId: "G-40ZLQMLCEE"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   });
 
   const db = firebaseApp.firestore();
